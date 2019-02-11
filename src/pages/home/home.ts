85986c04aa4c717cc0d9d73e39d76987f2875478
Pedro Paulo <pedropaulo19977@gmail.com>
2019-02-11 12:13:49 -0200

    Lazy Loading

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, IonicPage } from 'ionic-angular';
 
+@IonicPage() // pra falar que a classe é uma pagina, e eu referenciar a classe com String escrevendo o nome entre aspas
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
