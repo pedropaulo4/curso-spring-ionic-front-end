71f0ac68852777f059b29d20022fd88324f37980
Pedro Paulo <pedropaulo19977@gmail.com>
2019-02-18 14:01:25 -0300

    Salvando novo cliente

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,3 +1,4 @@
+import { FieldMessage } from './../models/fieldmessage';
 import { StorageService } from './../services/storage.services';
 
 import { HttpInterceptor, HttpRequest, HttpHandler, HTTP_INTERCEPTORS, HttpEvent } from "@angular/common/http";
@@ -45,6 +46,10 @@ export class ErrorInterceptor implements HttpInterceptor {
                 this.handle403();
                 break;
 
+                case 422:
+                this.handle422(errorObj);
+                break;
+
                 default: 
                 this.handleDefaultError(errorObj);
             }
@@ -57,6 +62,22 @@ export class ErrorInterceptor implements HttpInterceptor {
         this.storage.setLocalUser(null);
 
     }
+
+    handle422(errorObj) {
+        let alert = this.alertControler.create({
+            title: 'Erro 422: Validação',
+            message: this.listErrors(errorObj.errors),
+            enableBackdropDismiss: false, // pra sair do alert, tocar apenas no botão do alert
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+
+        alert.present();
+    }
+    
 
     handle401() {
         let alert = this.alertControler.create({
@@ -86,6 +107,15 @@ export class ErrorInterceptor implements HttpInterceptor {
 
         });
         alert.present();
+    }
+
+    listErrors(messages: FieldMessage[]): string {
+        let s : string = '';
+        for (var i=0; i<messages.length; i++) {
+            s = s + '<p><strong>' + messages[i].fieldName + '</strong><br>' + messages[i].message + '</p>';
+        }
+
+        return s;
     }
 
 }
--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,6 +1,7 @@
+import { ClienteService } from './../../services/domain/cliente.service';
 import { CidadeService } from './../../services/domain/cidade.service';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { FormGroup, FormBuilder, Validators} from '@angular/forms';
 import { EstadoService } from '../../services/domain/estado.service';
 import { CidadeDTO } from '../../models/cidade.dto';
@@ -29,7 +30,9 @@ export class SignupPage {
      public navParams: NavParams,
      public formBuilder: FormBuilder,
      public cidadeService : CidadeService,
-     public estadoService : EstadoService) { // para instanciar um formGroup
+     public estadoService : EstadoService,
+     public clienteService : ClienteService,
+     public alertCtrl: AlertController) { // para instanciar um formGroup
 // Para fazer a validação usando ReactForms
       this.formGroup = this.formBuilder.group({
         nome : ['Joaquin', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
@@ -72,7 +75,28 @@ export class SignupPage {
   }
     
   signupUser() {
-   console.log("enviou o form");
+      console.log(this.formGroup.value);
+      this.clienteService.insert(this.formGroup.value)
+      .subscribe(response => {
+        this.showInsertOk();
+      },
+      error => {});
+  }
+  showInsertOk(){
+    let alert = this.alertCtrl.create({
+      title: 'Sucesso!',
+      message: 'Cadastro efetuado com sucesso',
+      enableBackdropDismiss: false,
+      buttons: [
+        {
+          text: 'Ok',
+          handler: () => {
+            this.navCtrl.pop();
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 
 
